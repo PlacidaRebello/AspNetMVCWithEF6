92b0ddcf3da6a9bdf38ef6cf8da876d5aa17e79b
Placida Rebello <placidarebello12@gmail.com>
2020-07-30 01:42:52 +0530

    Initial Commit

---
+++ Test/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("TestEFDb")
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Test/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string  LName { get; set; }  
+    }
+}
